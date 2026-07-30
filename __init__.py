93bde63b6c9ed805d1fad63c33058182b3b176b5
Lucas Lorenz <lucas.lorenz@student.uibk.ac.at>
2026-07-31 04:53:54 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,3 @@
 """
-Initializes the gp_solver package.
 Insert documentation here.
 """
-
-
-#from gp_solver.core.grid        import Grid
-#from gp_solver.core.state       import GPState
-#from gp_solver.core.propagator  import SplitStepPropagator
-#from gp_solver.systems.scalar_gp  import ScalarGPSystem
